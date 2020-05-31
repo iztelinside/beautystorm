4b274637dd821dc66142b5f101ace3df2e821493
iztelinside <d.iztel@gmail.com>
2020-05-31 23:38:16 +0600

    latest

---
+++ js/script.js
@@ -0,0 +1,172 @@
+"use strict"
+// // let number = 5;
+// // const leftBorderLeft = 1;  
+
+
+// // number = 10;
+// // console.log(number);
+// // console.log("string"*9);
+
+
+// // leftBorderLeft = 10; 
+
+// // const bool = false;
+// // console.log(something);
+
+// // let und;
+// // console.log(und);
+
+// // let und;
+// // console.log(und);
+
+// // alert("Hello");
+
+// // const result = confirm("Are you here?");
+// // console.log(result);
+
+// // const answer = prompt("Вам есть 18?","18");
+// // console.log(answer);
+
+
+// // const category = "toys";
+
+// // http  nmnbmnb
+
+
+// const numberOfFilms = +prompt("Сколько фильмов мы просмотрели?","");
+// console.log(numberOfFilms);
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies:{},
+//     actors:{},
+//     genors:[],
+//     privat:false
+// };
+
+// // const a = prompt("Один из последних просмотренных фильмов?",""),
+// //       b = prompt("На сколько его оцените его?",""),
+// //       c = prompt("Один из последних просмотренных фильмов?",""),
+// //       d = prompt("На сколько его оцените его?","");
+
+// // personalMovieDB.movies[a] = b;
+// // personalMovieDB.movies[c] = d;
+
+
+
+
+
+
+// // console.log(personalMovieDB);
+
+
+// for (let i = 0; i < 2; i++){
+//     const a = prompt("Один из последних просмотренных фильмов?",""),
+//           b = prompt("На сколько его оцените его?","");
+          
+//     personalMovieDB.movies[a] = b;
+
+// }
+// console.log(personalMovieDB);
+
+// let incr = 10,
+//     decr = 10;
+
+// const numberOfFilms = +prompt('Сколько фильмов вы смотрели?','');
+
+// const personalMovieDB = {
+// count: numberOfFilms,
+//     movies:{},
+//     actors:{},
+//     genres:[],
+//     privat:false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?',''),
+//       b = prompt('На сколько вы его оценили?',''),
+//       c = prompt('Один из последних просмотренных фильмов?',''),
+//       d = prompt('На сколько вы его оценили?','');
+
+// personalMovieDB [a] = b;
+// personalMovieDB [c] = d;
+
+// if (4 == 4){
+//     console.log("Ti manbet");
+// } else {
+//     console.log("Error");
+// }
+
+// const num = 95;
+
+// if (num <= 90){
+//     console.log("Ты меньше 90");
+// } else if (num >= 100){
+//     console.log("Ты больше 100!");
+// } else {
+//     console.log("Superb");
+// }
+
+// (num === 100) ? console.log('Ok!') : console.log('Error!');
+
+// const num = 50; 
+
+
+// switch (num) {
+//     case 49: 
+//         console.log("Neverno");
+//         break;
+//     case 50:
+//         console.log("V tochku");
+//         break;
+//     default:
+//         console.log("Ne v etot raz");
+//         break;
+// };
+
+// const num  = 1000;
+
+// const numberOfFilms = +prompt('Сколько фильмов вы смотрели?','');
+
+// const personalMovieDB = {
+// count: numberOfFilms,
+//     movies:{},
+//     actors:{},
+//     genres:[],
+//     privat:false
+// };
+
+// // const a = prompt('Один из последних просмотренных фильмов?',''),
+// //       b = prompt('На сколько вы его оценили?',''),
+// //       c = prompt('Один из последних просмотренных фильмов?',''),
+// //       d = prompt('На сколько вы его оценили?','');
+
+// // personalMovieDB [a] = b;
+// // personalMovieDB [c] = d;
+
+// for(let i = 0; i < 2; i++){
+//     const a = prompt('Один из последних просмотренных фильмов?',''),
+//           b = prompt('На сколько вы его оценили?','');
+     
+//     if (a !=null && b != '' && a != '' && b!=null && b != '' && a.length<50){
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else{
+//         console.log('error');
+//         i--;
+//     }
+
+      
+//       personalMovieDB [a] = b;
+
+
+
+// }
+console.log(personalMovieDB.count);
+
+if (personalMovieDB.count <10){
+    console.log('У тебя меньше 10 фильмов)');
+} else if(personalMovieDB.count >= 10);{
+    console.log('У тебя между 10 и 30');
+} else if
+
+
